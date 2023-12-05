52021ab9ffe9986cff1d38ca86b9a30ceb194b14
Ignat <darkfourslife@gmail.com>
2023-12-06 02:00:09 +0300

    \change\ attribute listener looks a bit different

--- lib/src/listener.cpp
+++ lib/src/listener.cpp
@@ -24,64 +24,58 @@ listener::listener(net::io_context &ioc, tcp::endpoint endpoint,
                    std::shared_ptr<current_calls_d> state,
                    std::shared_ptr<ts_queue<std::shared_ptr<session>>> qptr)
     : ioc_(ioc), acceptor_(net::make_strand(ioc)), state_(state), q_ptr(qptr) {
-  BOOST_LOG_SEV(my_logger::get(), debug) << "listener:: Constructing...";
+  BOOST_LOG_SEV(lg, debug) << "[listener] Constructing...";
   beast::error_code ec;
-  BOOST_LOG_SEV(my_logger::get(), info) << "listener:: Opening the acceptor...";
+  BOOST_LOG_SEV(lg, info) << "[listener] Opening the acceptor...";
   acceptor_.open(endpoint.protocol(), ec);
   if (ec) {
-    BOOST_LOG_SEV(my_logger::get(), critical)
-        << "listener:: Open the acceptor " << ec.message();
+    BOOST_LOG_SEV(lg, critical)
+        << "[listener] Open the acceptor " << ec.message();
     return;
   } else {
-    BOOST_LOG_SEV(my_logger::get(), info)
-        << "listener:: Open the acceptor " << ec.message();
+    BOOST_LOG_SEV(lg, info) << "[listener] Open the acceptor " << ec.message();
   }
-  BOOST_LOG_SEV(my_logger::get(), info) << "listener:: Setting options...";
+  BOOST_LOG_SEV(lg, info) << "[listener] Setting options...";
   acceptor_.set_option(net::socket_base::reuse_address(true), ec);
   if (ec) {
-    BOOST_LOG_SEV(my_logger::get(), critical)
-        << "listener:: Setting options" << ec.message();
+    BOOST_LOG_SEV(lg, critical) << "[listener] Setting options" << ec.message();
     return;
   } else {
-    BOOST_LOG_SEV(my_logger::get(), info)
-        << "listener:: Setting options " << ec.message();
+    BOOST_LOG_SEV(lg, info) << "[listener] Setting options " << ec.message();
   }
-  BOOST_LOG_SEV(my_logger::get(), info)
-      << "listener:: Binding server address...";
+  BOOST_LOG_SEV(lg, info) << "[listener] Binding server address...";
   acceptor_.bind(endpoint, ec);
   if (ec) {
-    BOOST_LOG_SEV(my_logger::get(), critical)
-        << "listener:: Binding server address" << ec.message();
+    BOOST_LOG_SEV(lg, critical)
+        << "[listener] Binding server address" << ec.message();
     return;
   } else {
-    BOOST_LOG_SEV(my_logger::get(), info)
-        << "listener:: Binding server address " << ec.message();
+    BOOST_LOG_SEV(lg, info)
+        << "[listener] Binding server address " << ec.message();
   }
 
-  BOOST_LOG_SEV(my_logger::get(), info)
-      << "listener:: Starting listening for connections...";
+  BOOST_LOG_SEV(lg, info) << "[listener] Starting listening for connections...";
   acceptor_.listen(net::socket_base::max_listen_connections, ec);
   if (ec) {
-    BOOST_LOG_SEV(my_logger::get(), critical)
-        << "listener:: listening for connections..." << ec.message();
+    BOOST_LOG_SEV(lg, critical)
+        << "[listener] listening for connections..." << ec.message();
     return;
   } else {
-    BOOST_LOG_SEV(my_logger::get(), info)
-        << "listener:: listening for connections..." << ec.message();
+    BOOST_LOG_SEV(lg, info)
+        << "[listener] listening for connections..." << ec.message();
   }
 
-  BOOST_LOG_SEV(my_logger::get(), info) << "listener:: Construct Success";
+  BOOST_LOG_SEV(lg, info) << "[listener] Construct Success";
 }
 
 // Start accepting incoming connections
 void listener::run() {
-  BOOST_LOG_SEV(my_logger::get(), info) << "listener:: Running...";
+  BOOST_LOG_SEV(lg, info) << "[listener] Running...";
   std::thread t([this]() {
-    BOOST_LOG_SEV(my_logger::get(), info)
-        << "listener:: Make thread for queue handler";
+    BOOST_LOG_SEV(lg, info) << "[listener] Make thread for queue handler";
 
     for (;;) {
-      BOOST_LOG_SEV(my_logger::get(), debug) << "listener:: Call wait and pop";
+      BOOST_LOG_SEV(lg, debug) << "[listener] Call wait and pop";
       std::shared_ptr<session> sptr;
       q_ptr->wait_and_pop(sptr);
       auto opid = state_->join(sptr);
@@ -98,7 +92,7 @@ void listener::run() {
 }
 
 void listener::do_accept() {
-  BOOST_LOG_SEV(my_logger::get(), info) << "listener:: Accept new connection";
+  BOOST_LOG_SEV(lg, info) << "[listener] Accept new connection";
   // The new connection gets its own strand
   acceptor_.async_accept(
       net::make_strand(ioc_),
@@ -107,16 +101,16 @@ void listener::do_accept() {
 
 void listener::on_accept(beast::error_code ec, tcp::socket socket) {
   if (ec) {
-    BOOST_LOG_SEV(my_logger::get(), critical)
-        << "listener:: Can't Accept new connection" << ec.message();
+    BOOST_LOG_SEV(lg, critical)
+        << "[listener] Can't Accept new connection" << ec.message();
     return; // To avoid infinite loop
   } else {
-    BOOST_LOG_SEV(my_logger::get(), info)
-        << "listener:: Accepting new connection " << ec.message();
+    BOOST_LOG_SEV(lg, info)
+        << "[listener] Accepting new connection " << ec.message();
     auto now = boost::posix_time::second_clock::local_time();
 
     // Create the session and run it
-    BOOST_LOG_SEV(my_logger::get(), info) << "listener:: Making New session...";
+    BOOST_LOG_SEV(lg, info) << "[listener] Making New session...";
     std::make_shared<session>(std::move(socket), state_, to_simple_string(now),
                               q_ptr)
         ->run();
